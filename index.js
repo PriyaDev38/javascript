e7cf26c816685ac2e3b30bbc28b5c7d302b9c8fe
PriyaDev38 <oedzpriya1@gmail.com>
2024-06-12 13:08:40 +0545

    Learn Javascript

---
+++ index.js
@@ -0,0 +1,57 @@
+// datatypes
+// number - 1,2,3,4
+// string -"N9solution",'N9solution'
+// boolean- True or falsse
+// Operators-mathematical operation; +,-,*,/
+
+// console.log("Hello");
+// console.log("This is me Priya");
+// console.log("I am in N9Solution");
+// console.log("At Chabhail");
+
+// console.log(5+6);
+// console.log(5*4);
+
+// console.log("a"+"b");
+
+// Javascript is a asynchchronous and single threaded language.
+// If two numbers are added it produces a output of sum and if two strings are added it concatentaion and produces outputs as a string.
+
+
+// console.log("Priya");--For string
+// console.log('Priya');
+// console.log(`Priya`);
+
+/* hjhdjh
+bcjbc
+djb */
+
+// Variaable---is storage
+// var, let, const
+
+// let name="priya"
+// console.log(name);
+
+// let address="Ktm"
+
+// let age="50"
+// age="30"
+// console.log(age);
+
+// let num1=1;
+// let num2=2;
+// console.log(num1+num2);
+
+// let num1=5;
+// let num2=6;
+// console.log(`sum of ${num1}`);---This is template literal
+// console.log("a"-"a");---NAN
+
+let  name="Priya"
+console.log(name);
+
+let age="10"
+console.log(age);
+
+let address="ktm"
+console.log(address);
