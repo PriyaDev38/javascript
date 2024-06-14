ba43f6bd46f7ab4403a739b00db1b9fd12dde2f5
PriyaDev38 <oedzpriya1@gmail.com>
2024-06-14 11:23:00 +0545

    Learn Javascript

--- index.js
+++ index.js
@@ -44,19 +44,123 @@ djb */
 
 // let num1=5;
 // let num2=6;
-// console.log(`sum of ${num1}`);---This is template literal
+// console.log(`sum of ${num1}`);---if something is written in back tick then This is template literal
 // console.log("a"-"a");---NAN
 
-let  name="Priya"
-console.log(name);
+// let  name="Priya"
+// console.log(name);
 
-let age="10"
-console.log(age);
+// let age="10"
+// console.log(age);
 
-let address="ktm"
-console.log(address);
+// let address="ktm"
+// console.log(address);
 
-let num1=5;
-let num2=1;
-console.log(num1+num2);
+// let num1=5;
+// let num2=1;
+// console.log(num1+num2);
 
+// string
+// example
+
+// let a="Priya";
+// console.log(a);
+
+// let a="pri'ya";---we can use a single single in between of double straing and vice-veras and cant use the same in between
+// console.log(1+2+3+"4");
+// 1+2
+// 3+3
+// 6+"4"
+// 64
+
+// console.log("1"+2+3+4);=1234 because string always go from left to right
+// If we add a number and StaticRange, the interpreter convers the number into a string and concantentates both together
+
+// let address="ktm";
+// let address="bhakatpur";
+// console.log(address);
+// we cannot declare variable name twice-it throws error
+
+// String literals-``
+// let num1=1;
+// let num2=2;
+// let message=`${num1}`;
+// console.log(message);
+
+// next
+
+// let num1=1;
+// let num2=2;
+// let message=`${num1}`;
+// let sum=`the sum of ${num1} and ${num2} is ${num1+num2}`;
+// console.log(message);console.log(sum);
+
+// let name="Priya";
+// let surname="Dev";
+// let fullname=`my name is ${name} ${surname}`; 
+// console.log(fullname);
+
+// truthy or falsy value
+// boolean true of false
+// != Not-falsy value
+// single string is falsy value other are truthy value
+// 0 is also a falsy value others true
+// let a= !!"false";----false is also a value so it is true
+// console.log(a);
+
+
+// ===, >,<, >==, <==
+// console.log(1===2);
+
+// if else
+// let a=true;
+// if(a){
+//     console.log("i am if");
+// }
+//     else {
+//         console.log("i am else");
+
+//     }
+// in if else statement condition block is always wrapped by parenthesis
+// ()=> parenthesis
+// {}   curly bracets
+// []  array
+// let name="priya";
+// if(name==="kekiii"){
+//     console.log("if am a");
+// } 
+// else if(name==="bweee"){
+//     console.log("if am b");
+// }
+// else if(name==="sita"){
+//     console.log("if am c");
+// }
+// else (name==="gita")
+// {
+//     console.log("if am other");
+// }
+
+
+// let name=10
+// if(name>1){
+//     console.log("i am a");
+// }
+// else if(name>=11){
+//     console.log("i am b");
+// }
+// else {
+//     console.log("i am c");
+// }
+
+
+// if the value is true single Not (!) makes it false
+// double not (!!) converts the value into boolean
+//  let name="priya";
+//  if (!name){
+//     console.log("a");
+//  }
+//  else {
+//     console.log("b");
+//  }
+
+// function
